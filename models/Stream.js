8dc789b9ea33d9b957cf48376fabeaf0c1634cfa
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-03-18 22:29:00 +0530

    added Stream Model

---
+++ models/Stream.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema();
+
+const StreamSchema = new Schema({
+  title: String,
+  description: String,
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Stream = mongoose.model("Stream", StreamSchema);
