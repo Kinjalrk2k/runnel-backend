560a356fed7a4c33724e3a7b3bb4567e6fb256a6
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-03-20 17:40:15 +0530

    added basic Stream apis and model

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # runnel-backend
+
+## Endpoints
+
+| Action                    | Method | Route        | Response         |
+| ------------------------- | ------ | ------------ | ---------------- |
+| List all records          | GET    | /streams     | Array of Records |
+| Get one particular record | GET    | /streams/:id | Single Record    |
+| Create record             | POST   | /streams     | Single Record    |
+| Update a record           | PUT    | /streams/:id | Single Record    |
+| Delete a record           | DELETE | /streams/:id | Nothing          |
--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const db = process.env.mongoURI;
 const app = express();
+
+app.use(cors());
 
 // Express BodyParser
 app.use(express.json());
@@ -21,10 +25,12 @@ mongoose
 
 app.get("/", (req, res) => {
   res.json({
-    app: "Reunnel",
+    app: "Runnel",
     developer: "Kinjal Raykarmakar",
   });
 });
+
+app.use("/streams", require("./routes/streams"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
